bada50dd56710a0d4b510d9e39643f90b1de0958
MRM3UK <mynewp570@gmail.com>
2025-06-20 16:30:34 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Telegram Bot Token
+TOKEN = "7989339593:AAETPRFdQjVem1BmAv0RpJcGoT3_OY05JQM"
+
+# Log Channel ID (optional, use None if not using)
+LOG_CHANNEL = -1001234567890
+
+# Max download file size (in bytes)
+MAX_FILESIZE = 50 * 1024 * 1024  # 50MB
